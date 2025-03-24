1dae210576f61081d1c5c2c325e4b328bf601a2b
Natt_Pissanu <pissanu@bakertilly.co.th>
2025-03-24 10:48:03 +0700

    initialize project

---
+++ src/main/java/com/kpi_service/kpi_service/KpiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kpi_service.kpi_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kpi_service/kpi_service/KpiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpi_service.kpi_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
